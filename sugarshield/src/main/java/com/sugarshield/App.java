4e289cb4acf714e90580257fc1415a6ad1cc34a1
abubakar Kaleem <abubakarkaleem22@gmail.com>
2026-05-10 14:40:09 +0500

    typo error

--- sugarshield/src/main/java/com/sugarshield/App.java
+++ sugarshield/src/main/java/com/sugarshield/App.java
@@ -2,6 +2,6 @@ package com.sugarshield;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Sugar Shield");
+        System.out.println("Hello Sugar Shield ");
     }
 }
