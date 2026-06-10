a822b5d6b3c15611ecafce40175de325a63bcdb0
abubakar Kaleem <abubakarkaleem22@gmail.com>
2026-06-10 19:04:06 +0500

    port update

--- sugarshield/src/main/java/com/sugarshield/db/DBConnection.java
+++ sugarshield/src/main/java/com/sugarshield/db/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:33061/sugarshield";
+    private static final String URL = "jdbc:mysql://localhost:3306/sugarshield";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
